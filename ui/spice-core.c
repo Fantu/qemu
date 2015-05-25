8e48c4d41b9e8ba07bc695d8389ad0b51108e603
Fabio Fantoni <fabio.fantoni@m2r.biz>
2015-05-25 16:15:23 +0200

    Add spice LZ4 compression support

--- ui/spice-core.c
+++ ui/spice-core.c
@@ -347,6 +347,9 @@ static const char *compression_names[] = {
     [ SPICE_IMAGE_COMPRESS_QUIC ]     = "quic",
     [ SPICE_IMAGE_COMPRESS_GLZ ]      = "glz",
     [ SPICE_IMAGE_COMPRESS_LZ ]       = "lz",
+#if SPICE_SERVER_VERSION >= 0x000c07
+    [ SPICE_IMAGE_COMPRESS_LZ4 ]      = "lz4",
+#endif
 };
 #define parse_compression(_name)                                        \
     parse_name(_name, "image compression",                              \
